af579c1c6ada81c062def42e0a666e788498e5c1
Nishith-S-Dev <nishith.s.acharya@gmail.com>
2025-08-12 22:03:36 +0530

    in this addition of the components for the required on the project are added using the shadCn

---
+++ vechile/app/page.js
@@ -0,0 +1,12 @@
+
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <br />
+    <Button variant="destructive">Click me</Button>
+    </>
+  )
+  
+}
